7957a2f517534fa4a62254c040e88d5101a3f166
Zoe <Zoe.kang@travelflan.com>
2021-02-10 14:40:34 +0900

    2021-02-10-14:40

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import post_list, post_detail
 urlpatterns = [
-    path('list', post_list),
-    path('detail', post_detail),
+    path('list/', post_list, name='list'),
+    path('detail/', post_detail, name='detail'),
 ]
